55d662f3f4e47e02e1832af738c931db7d33d11c
Salvador Zendejas <salvador.zendejas@gmail.com>
2023-08-26 14:42:49 -0500

    RPItOS [RtOS-2] - Initial Essential Cfg
    - common datatypes

---
+++ include/common.h
@@ -0,0 +1,27 @@
+/**
+ * @file common.h
+ * @author Salvador Z
+ * @version 1.0
+ * @brief File for common files
+ *
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+// Include
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef volatile u32 reg32;
+
+#endif /* COMMON_H_ */
