485c6fd43f5eed4a11418e45a113527d3020480d
Salvador Zendejas <salvador.zendejas@gmail.com>
2023-08-27 21:16:04 -0500

    RPItOS [RtOS-3] - GPIO Initial Cfg
    - peripherals register map

---
+++ include/peripherals/gpio_reg_bcm2711.h
@@ -0,0 +1,48 @@
+/**
+ * @file gpio_reg.h
+ * @author Salvador Z
+ * @version 1.0
+ * @brief File for GPIO Register Map for BCM2711
+ *
+ */
+
+#ifndef GPIO_REG_BCM2711_H_
+#define GPIO_REG_BCM2711_H_
+
+// Includes
+#include "common.h"
+
+#include "base_addr.h"
+
+#define GPIO_FUNC_REG_MAX   (6U) /* GPFSEL0-5*/
+#define GPIO_FSEL_BIT_MAX   (30U) /* FSEL BIT FIELD MAX n-PIN Cfg per Register*/
+#define GPIO_FSEL_BIT_GRP   (3U)  /* 3 bits group for FSEL Cfg*/
+#define GPIO_FSEL_BIT_MSK   ((1 << GPIO_FSEL_BIT_GRP) - 1) /* FSELn Bit field Mask*/
+#define GPIO_PUPD_CTL_MAX   (4U)  /* GPIO_PUP_PDN_CNTRL_REG0-3 */
+#define GPIO_PUPD_PIN_GRP   (16U) /* GPIO PUPPD MAX nPin per Register */
+#define AUX_REGISTER_OFFSET (0x00200000)
+
+struct GPIO_CFG {
+  reg32 const RESERVED;
+  reg32 N[2];
+};
+
+struct GPIO_REG {
+  reg32 fselN[GPIO_FUNC_REG_MAX];
+  struct GPIO_CFG output_set;
+  struct GPIO_CFG output_clear;
+  struct GPIO_CFG level;
+  struct GPIO_CFG ev_detect_status;
+  struct GPIO_CFG re_detect_enable;
+  struct GPIO_CFG fe_detect_enable;
+  struct GPIO_CFG hi_detect_enable;
+  struct GPIO_CFG lo_detect_enable;
+  struct GPIO_CFG async_re_detect;
+  struct GPIO_CFG async_fe_detect;
+  reg32 const reserved[21];
+  reg32 GPPUD_CNTRL[GPIO_PUPD_CTL_MAX];
+};
+
+#define GPIO_REG_MAP ((struct GPIO_REG *)(PBASE + 0x00200000))
+
+#endif /* GPIO_REG_BCM2711_H_ */
---
+++ include/peripherals/gpio_reg_bcm283x.h
@@ -0,0 +1,49 @@
+/**
+ * @file gpio_reg_bcm283x.h
+ * @author Salvador Z
+ * @version 1.0
+ * @brief File for GPIO Register Map on BCM283x
+ *
+ */
+
+#ifndef GPIO_REG_BCM283X_H_
+#define GPIO_REG_BCM283X_H_
+
+// Includes
+#include "common.h"
+
+#include "base_addr.h"
+
+#define GPIO_FUNC_REG_MAX   (6U)  /* GPFSEL0-5*/
+#define GPIO_FSEL_BIT_MAX   (30U) /* FSEL BIT FIELD MAX n-PIN Cfg per Register*/
+#define GPIO_FSEL_BIT_GRP   (3U)  /* 3 bits group for FSEL Cfg*/
+#define GPIO_FSEL_BIT_MSK   ((1 << GPIO_FSEL_BIT_GRP) - 1) /* FSELn Bit field Mask*/
+#define GPIO_PUDCLKn_MAX    (32u) /* PUDCLKn 0 - 31 and 32-53*/
+#define GPIO_WAIT_TICKs     (150U)
+#define AUX_REGISTER_OFFSET (0x00200000)
+
+struct GPIO_CFG {
+  reg32 const RESERVED;
+  reg32 N[2];
+};
+
+struct GPIO_REG {
+  reg32 fselN[GPIO_FUNC_REG_MAX];
+  struct GPIO_CFG output_set;
+  struct GPIO_CFG output_clear;
+  struct GPIO_CFG level;
+  struct GPIO_CFG ev_detect_status;
+  struct GPIO_CFG re_detect_enable;
+  struct GPIO_CFG fe_detect_enable;
+  struct GPIO_CFG hi_detect_enable;
+  struct GPIO_CFG lo_detect_enable;
+  struct GPIO_CFG async_re_detect;
+  struct GPIO_CFG async_fe_detect;
+  reg32 const reserved;
+  reg32 GPPUD_En;
+  reg32 GPPUDCLK_En[2];
+};
+
+#define GPIO_REG_MAP ((struct GPIO_REG *)(PBASE + 0x00200000))
+
+#endif /* GPIO_REG_BCM283X_H_ */
